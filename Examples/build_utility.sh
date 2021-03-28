671dcf0880e8d94db8264e8b6bd0e7acd2e7f448
Narisok <dimedrov123@gmail.com>
2021-03-28 22:52:42 +0300

    scripts updated

--- Examples/build_utility.sh
+++ Examples/build_utility.sh
@@ -3,4 +3,5 @@
 mkdir -p Build/Examples
 touch Build/.gitignore
 echo "*" > Build/.gitignore
+
 gcc -O2 Examples/utility.c base64.c -o Build/Examples/utility
--- Tests/run_test.sh
+++ Tests/run_test.sh
@@ -1,6 +1,9 @@
 #!bin/bash
 
 mkdir -p ./Build/Tests
+touch Build/.gitignore
+echo "*" > Build/.gitignore
+
 rm -f ./Build/Tests/cur.test
 gcc -g ./Tests/run.c ./base64.c -o ./Build/Tests/cur.test
 
